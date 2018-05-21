f867cf84deeba31b603d6be149a7a95984740f7b
Security Chops <39027035+securitychops@users.noreply.github.com>
2018-05-21 12:38:51 -0500

    updating to dump file in tmp directory

--- assignment-2/shellcode-dump.sh
+++ assignment-2/shellcode-dump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd $1
 
-objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+objdump -d ./$2|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
